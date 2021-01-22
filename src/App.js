de107bc19529b97b14c8a182a3b17ea81a2833a3
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-01-22 09:15:46 -0800

    forgot to commit, finished mvp, working on styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
+import { BASE_URL } from "./constants/index";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [data, setData] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios 
+      .get(`${BASE_URL}`)
+      .then(res => {
+       setData(
+         res.data.results.map((people) =>{
+            return people
+          })
+        )
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character data={data}></Character>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,48 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Character = (props) => {
+    const { data } = props;
+
+
+    return (
+        <StyledContainer>
+        {data.map(char => { 
+            return <StyledCharacter>
+                        {char.name}
+                        <StyledStats>
+                            Height: {char.height},
+                            Born: {char.birth_year}
+                        </StyledStats>
+                    </StyledCharacter>})}
+        </StyledContainer>
+  )  
+}
+
+
+
+export default Character;
+
+const StyledContainer = styled.div ` 
+    border: 3px solid grey;
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+    background-color: #C3AF97;
+    
+    
+`
+
+const StyledCharacter = styled.div `
+    border: 2px solid black;
+    font-size: 1.2rem;
+    padding: 2%;
+    
+`
+
+const StyledStats = styled.div ` 
+    font-size: 1rem;
+    border: 2px solid red;
+`
